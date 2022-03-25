63af3e9e6a3601a16e0bd6e655be799eebf1f335
Duna Marius <marius.duna@cujo.com>
2022-03-25 15:01:25 +0200

    Calling Kotlin function from C++
    Calling Kotlin function from C++

--- app/src/main/java/com/ndk/bi/MainActivity.kt
+++ app/src/main/java/com/ndk/bi/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.ndk.bi
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +16,8 @@ class MainActivity : AppCompatActivity() {
      * Native functions
      */
     private external fun stringFromJNI(): String?
+    private external fun concatStr(str: String): String?
+    private external fun concatStrParam(str: String): String?
 
     /**
      * Loading the native library in runtime
@@ -23,19 +28,29 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var han = Handler(Looper.getMainLooper())
         setContentView(R.layout.activity_main)
         val cButton = findViewById<Button>(R.id.button_call_c)
         cButton.setOnClickListener(View.OnClickListener {
             cButton.text = stringFromJNI()
+            Toast.makeText(this, concatStr("Toast str: "), Toast.LENGTH_LONG).show()
+            han.postDelayed(Runnable {
+                Toast.makeText(this, concatStrParam("Toast str: "), Toast.LENGTH_LONG).show()
+            }, 3000)
         })
     }
 
     /**
      * Expose the method to be called from c code
      */
-    fun obtainId(value: String): String {
+    fun obtainId(): String {
+        Log.e(TAG, "Value received from native code")
+        return "ID - null"
+    }
+
+    fun obtainIdParam(value: String): String {
         Log.e(TAG, "Value received from native code: $value")
-        return "ID: $value"
+        return "ID - $value"
     }
 
 
